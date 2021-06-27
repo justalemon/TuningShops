f29f7131f2500271aed4a7714891b14b2bfde914
Hannele Ruiz <justlemoncl@gmail.com>
2021-06-27 02:21:01 -0400

    Fixed toggle menu being used for general mods

--- TuningShops/Menus/MenuToggle.cs
+++ TuningShops/Menus/MenuToggle.cs
@@ -1,8 +1,6 @@
 ﻿using GTA;
 using GTA.Native;
 using LemonUI.Menus;
-using System;
-using System.ComponentModel;
 
 namespace TuningShops.Menus
 {
@@ -22,33 +20,12 @@ namespace TuningShops.Menus
 
         #region Constructors
 
-        public MenuToggle(int slot) : base(Function.Call<string>(Hash.GET_MOD_SLOT_NAME, Game.Player.Character.CurrentVehicle, slot))
+        public MenuToggle(int slot) : base("", Function.Call<string>(Hash.GET_MOD_SLOT_NAME, Game.Player.Character.CurrentVehicle, slot))
         {
             Slot = slot;
-            Opening += MenuToggle_Opening;
-        }
 
-        #endregion
-
-        #region Events
-
-        private void MenuToggle_Opening(object sender, CancelEventArgs e)
-        {
-            Clear();
-
-            Vehicle vehicle = Game.Player.Character.CurrentVehicle;
-
-            if (vehicle == null)
-            {
-                return;
-            }
-            
-            int count = Function.Call<int>(Hash.GET_NUM_VEHICLE_MODS, vehicle, Slot);
-
-            for (int i = 0; i < count; i++)
-            {
-                Add(new ItemSwap(Slot, i));
-            }
+            Add(new ItemToggle(slot, false));
+            Add(new ItemToggle(slot, true));
         }
 
         #endregion
