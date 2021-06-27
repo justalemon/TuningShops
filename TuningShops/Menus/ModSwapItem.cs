14c464024856878d052834d2581dda4df99f4d75
Hannele Ruiz <justlemoncl@gmail.com>
2021-06-27 01:46:17 -0400

    Added menu item to select a specific item

---
+++ TuningShops/Menus/ModSwapItem.cs
@@ -0,0 +1,54 @@
+﻿using GTA;
+using GTA.Native;
+using LemonUI.Menus;
+using System;
+
+namespace TuningShops.Menus
+{
+    /// <summary>
+    /// The item used to select a different item.
+    /// </summary>
+    public class ModSwapItem : NativeItem
+    {
+        #region Properties
+
+        /// <summary>
+        /// The type of mod.
+        /// </summary>
+        public int Type { get; }
+        /// <summary>
+        /// The value of the mod for the type.
+        /// </summary>
+        public int Value { get; }
+
+        #endregion
+
+        #region Constructor
+
+        public ModSwapItem(int type, int value) : base(Function.Call<string>(Hash._GET_LABEL_TEXT, Function.Call<string>(Hash.GET_MOD_TEXT_LABEL, Game.Player.Character.CurrentVehicle, type, value)))
+        {
+            Type = type;
+            Value = value;
+
+            Activated += ModSwapItem_Activated;
+        }
+
+        #endregion
+
+        #region Events
+
+        private void ModSwapItem_Activated(object sender, EventArgs e)
+        {
+            Vehicle vehicle = Game.Player.Character.CurrentVehicle;
+
+            if (vehicle == null)
+            {
+                return;
+            }
+
+            Function.Call(Hash.SET_VEHICLE_MOD, vehicle, Type, Value, false);
+        }
+
+        #endregion
+    }
+}
