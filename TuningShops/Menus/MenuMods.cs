dbea5bf49a138c6cf3ce8eef23c3126aaeeecb16
Hannele Ruiz <justlemoncl@gmail.com>
2021-06-27 02:25:28 -0400

    Added menu for selecting specific mods

---
+++ TuningShops/Menus/MenuMods.cs
@@ -0,0 +1,38 @@
+﻿using GTA;
+using GTA.Native;
+using System.ComponentModel;
+
+namespace TuningShops.Menus
+{
+    /// <summary>
+    /// The menu that allows you to change the different mods of the vehicle.
+    /// </summary>
+    public class MenuMods : MenuBase
+    {
+        #region Constructors
+
+        public MenuMods(int slot) : base(slot)
+        {
+            Opening += MenuMods_Opening;
+        }
+
+        private void MenuMods_Opening(object sender, CancelEventArgs e)
+        {
+            Vehicle vehicle = Game.Player.Character.CurrentVehicle;
+
+            if (vehicle == null)
+            {
+                return;
+            }
+
+            int count = Function.Call<int>(Hash.GET_NUM_VEHICLE_MODS, vehicle, Slot);
+
+            for (int i = 0; i < count; i++)
+            {
+                Add(new ItemSwap(Slot, i));
+            }
+        }
+
+        #endregion
+    }
+}
