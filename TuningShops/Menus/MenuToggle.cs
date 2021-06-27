c5da1906302b55db7a9c45c23cf1bd23a5e96fe6
Hannele Ruiz <justlemoncl@gmail.com>
2021-06-27 02:09:15 -0400

    Added menu to change a specific slot of vehicle mods

---
+++ TuningShops/Menus/MenuToggle.cs
@@ -0,0 +1,56 @@
+﻿using GTA;
+using GTA.Native;
+using LemonUI.Menus;
+using System;
+using System.ComponentModel;
+
+namespace TuningShops.Menus
+{
+    /// <summary>
+    /// Menu used to toggle items on or off.
+    /// </summary>
+    public class MenuToggle : NativeMenu
+    {
+        #region Properties
+
+        /// <summary>
+        /// The slot managed by this menu.
+        /// </summary>
+        public int Slot { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public MenuToggle(int slot) : base(Function.Call<string>(Hash.GET_MOD_SLOT_NAME, Game.Player.Character.CurrentVehicle, slot))
+        {
+            Slot = slot;
+            Opening += MenuToggle_Opening;
+        }
+
+        #endregion
+
+        #region Events
+
+        private void MenuToggle_Opening(object sender, CancelEventArgs e)
+        {
+            Clear();
+
+            Vehicle vehicle = Game.Player.Character.CurrentVehicle;
+
+            if (vehicle == null)
+            {
+                return;
+            }
+            
+            int count = Function.Call<int>(Hash.GET_NUM_VEHICLE_MODS, vehicle, Slot);
+
+            for (int i = 0; i < count; i++)
+            {
+                Add(new ItemSwap(Slot, i));
+            }
+        }
+
+        #endregion
+    }
+}
