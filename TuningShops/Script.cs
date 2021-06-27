0918ba2ccc81a959acd8906968fd371e656b18b4
Hannele Ruiz <justlemoncl@gmail.com>
2021-06-27 01:32:11 -0400

    Added basic script class

---
+++ TuningShops/Script.cs
@@ -0,0 +1,36 @@
+﻿using GTA;
+using LemonUI;
+using System;
+
+namespace TuningShops
+{
+    /// <summary>
+    /// The main class that handles the Tuning Shop features.
+    /// </summary>
+    public class TuningShops : Script
+    {
+        #region Fields
+
+        private static readonly ObjectPool pool = new ObjectPool();
+
+        #endregion
+
+        #region Constructor
+
+        public TuningShops()
+        {
+            Tick += TuningShops_Tick;
+        }
+
+        #endregion
+
+        #region Events
+
+        private void TuningShops_Tick(object sender, EventArgs e)
+        {
+            pool.Process();
+        }
+
+        #endregion
+    }
+}
