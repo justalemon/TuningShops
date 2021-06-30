50f14ec60eebb55f9c89e705a578b31c8bc1f9ce
Hannele Ruiz <justlemoncl@gmail.com>
2021-06-30 04:27:54 -0400

    Added main menu for testing stuff

---
+++ TuningShops/MainMenu.cs
@@ -0,0 +1,81 @@
+﻿using GTA;
+using LemonUI.Menus;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TuningShops.Base;
+
+namespace TuningShops
+{
+    /// <summary>
+    /// Represents a main menu in a specific location.
+    /// </summary>
+    public class MainMenu : NativeMenu
+    {
+        #region Fields
+
+        private readonly List<BaseType> menus = new List<BaseType>();
+        private Model lastModel = 0;
+
+        #endregion
+
+        #region Constructor
+
+        public MainMenu() : base("", "Test")
+        {
+            UseMouse = false;
+            Opening += MainMenu_Opening;
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Adds a specific menu as part of this location.
+        /// </summary>
+        /// <param name="menu">The menu to add.</param>
+        public void AddMenu(BaseType menu)
+        {
+            if (menus.Contains(menu))
+            {
+                return;
+            }
+
+            menus.Add(menu);
+        }
+
+        #endregion
+
+        #region Events
+
+        private void MainMenu_Opening(object sender, CancelEventArgs e)
+        {
+            Vehicle vehicle = Game.Player.Character.CurrentVehicle;
+            Model model = vehicle != null ? vehicle.Model : new Model(0);
+
+            if (model == lastModel)
+            {
+                return;
+            }
+
+            Clear();
+
+            if (vehicle == null)
+            {
+                return;
+            }
+
+            foreach (BaseType menu in menus)
+            {
+                if (menu.CanUse(vehicle))
+                {
+                    AddSubMenu(menu);
+                }
+            }
+
+            lastModel = model;
+        }
+
+        #endregion
+    }
+}
