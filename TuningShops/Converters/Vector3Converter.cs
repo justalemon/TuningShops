f1adceda1441aa2f7187af3b723dc5b4062dd2ca
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-10 13:28:28 -0400

    Added converter for Vector3

---
+++ TuningShops/Converters/Vector3Converter.cs
@@ -0,0 +1,44 @@
+﻿using GTA.Math;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace TuningShops.Converters
+{
+    /// <summary>
+    /// Converts a Vector3 to JSON and vice versa.
+    /// </summary>
+    public class Vector3Converter : JsonConverter<Vector3?>
+    {
+        public override Vector3? ReadJson(JsonReader reader, Type objectType, Vector3? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+
+            JObject @object = JObject.Load(reader);
+            float x = @object.ContainsKey("x") ? (float)@object["x"] : 0;
+            float y = @object.ContainsKey("y") ? (float)@object["y"] : 0;
+            float z = @object.ContainsKey("z") ? (float)@object["z"] : 0;
+            return new Vector3(x, y, z);
+        }
+
+        public override void WriteJson(JsonWriter writer, Vector3? value, JsonSerializer serializer)
+        {
+            if (!value.HasValue)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            JObject @object = new JObject
+            {
+                ["x"] = value.Value.X,
+                ["y"] = value.Value.Y,
+                ["z"] = value.Value.Z
+            };
+            @object.WriteTo(writer);
+        }
+    }
+}
