e2290a4c2e18c9daaf2f3b8830451bc74717dfa0
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-11 14:18:01 -0400

    Added Slot for the Pearlescent colors

---
+++ TuningShops/Slots/ColorPearlescent.cs
@@ -0,0 +1,18 @@
+﻿using TuningShops.Core;
+
+namespace TuningShops.Slots
+{
+    /// <summary>
+    /// Applies Pearlescent colors to the primary slot.
+    /// </summary>
+    public class ColorPearlescent : ColorClassic
+    {
+        #region Constructor
+
+        public ColorPearlescent() : base(ColorSlot.Pearlescent, "")
+        {
+        }
+
+        #endregion
+    }
+}
