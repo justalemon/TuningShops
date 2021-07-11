93f8300ef0a550a22160e966b34334d138cb0652
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-11 15:45:16 -0400

    Added wheel color option

---
+++ TuningShops/Slots/ColorWheels.cs
@@ -0,0 +1,16 @@
+﻿namespace TuningShops.Slots
+{
+    /// <summary>
+    /// Applies Wheel colors to the primary slot.
+    /// </summary>
+    public class ColorWheels : Core.ColorWheels
+    {
+        #region Constructor
+
+        public ColorWheels() : base(ColorSlot.Wheels)
+        {
+        }
+
+        #endregion
+    }
+}
