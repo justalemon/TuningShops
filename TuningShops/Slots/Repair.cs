c4fa2f45311c3ff304d378859a5463fd6d7b087d
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-24 02:32:45 -0400

    Added menu to Repair the vehicle

---
+++ TuningShops/Slots/Repair.cs
@@ -0,0 +1,38 @@
+﻿using GTA;
+using LemonUI.Menus;
+using TuningShops.Core;
+
+namespace TuningShops.Slots
+{
+    /// <summary>
+    /// Menu option used to repair the vehicle.
+    /// </summary>
+    public class Repair : BaseType
+    {
+        #region Constructor
+
+        public Repair() : base("Repair Vehicle")
+        {
+            NativeItem item = new NativeItem("Repair");
+            item.Activated += (sender, e) => Game.Player.Character.CurrentVehicle?.Repair();
+            Add(item);
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <inheritdoc/>
+        public override bool CanUse(Vehicle vehicle) => true;
+        /// <inheritdoc/>
+        public override void Repopulate()
+        {
+        }
+        /// <inheritdoc/>
+        public override void SelectCurrent(Vehicle vehicle)
+        {
+        }
+
+        #endregion
+    }
+}
