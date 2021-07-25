b0e9003caa6913889cb0413e73cb02d0626b498b
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-25 19:57:56 -0400

    Allow individual vehicle models to override specific cameras

--- TuningShops/Cameras/CameraManager.cs
+++ TuningShops/Cameras/CameraManager.cs
@@ -1,4 +1,5 @@
-﻿using GTA.UI;
+﻿using GTA;
+using GTA.UI;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,7 @@ namespace TuningShops.Cameras
 
         private static readonly Dictionary<string, Guid> associations = new Dictionary<string, Guid>();
         private static readonly Dictionary<Guid, CameraCore> cameras = new Dictionary<Guid, CameraCore>();
+        private static readonly Dictionary<Model, Dictionary<string, Guid>> overrides = new Dictionary<Model, Dictionary<string, Guid>>();
 
         #endregion
 
@@ -71,6 +73,61 @@ namespace TuningShops.Cameras
                 Notification.Show($"~o~Warning~s~: Unable to load Camera Associations:\n{ex.Message}");
             }
         }
+        private static void PopulateSpecificOverrides(string path)
+        {
+            if (!path.ToLowerInvariant().EndsWith(".json"))
+            {
+                Notification.Show($"~o~Warning~s~: File {path} is not a JSON File!");
+                return;
+            }
+
+            try
+            {
+                string contents = File.ReadAllText(path);
+                List<CameraOverride> @new = JsonConvert.DeserializeObject<List<CameraOverride>>(contents);
+
+                foreach (CameraOverride @override in @new)
+                {
+                    Dictionary<string, Guid> dict;
+
+                    if (overrides.ContainsKey(@override.Model))
+                    {
+                        dict = overrides[@override.Model];
+                    }
+                    else
+                    {
+                        dict = new Dictionary<string, Guid>();
+                        overrides.Add(@override.Model, dict);
+                    }
+
+                    foreach (KeyValuePair<string, Guid> newOverride in @override.Overrides)
+                    {
+                        if (dict.ContainsKey(newOverride.Key))
+                        {
+                            Notification.Show($"~o~Warning~s~: Model {@override.Model} already has an Override for {newOverride.Key}!");
+                            continue;
+                        }
+
+                        dict.Add(newOverride.Key, newOverride.Value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Notification.Show($"~o~Warning~s~: Unable to load Camera Override:\n{ex.Message}");
+            }
+        }
+        private static void PopulateOverrides()
+        {
+            overrides.Clear();
+
+            string path = Path.Combine(TuningShops.location, "Cameras", "Overrides");
+
+            foreach (string file in Directory.EnumerateFiles(path))
+            {
+                PopulateSpecificOverrides(file);
+            }
+        }
 
         #endregion
 
@@ -86,6 +143,7 @@ namespace TuningShops.Cameras
             PopulateSpecific<BoneCameraPosition>(Path.Combine(TuningShops.location, "Cameras", "Bone"));
             PopulateSpecific<ModCameraPosition>(Path.Combine(TuningShops.location, "Cameras", "Mod"));
             PopulateAssociations();
+            PopulateOverrides();
         }
         /// <summary>
         /// Gets a Camera with a specific ID.
@@ -98,10 +156,15 @@ namespace TuningShops.Cameras
         /// <param name="base">The menu to use as a base.</param>
         public static CameraCore Get(BaseType @base)
         {
+            Model model = Game.Player.Character.CurrentVehicle.Model;
             Type type = @base.GetType();
             string name = type.FullName;
 
-            if (associations.TryGetValue(name, out Guid guid))
+            if (overrides.ContainsKey(model) && overrides[model].TryGetValue(name, out Guid @override))
+            {
+                return Get(@override);
+            }
+            else if (associations.TryGetValue(name, out Guid guid))
             {
                 return Get(guid);
             }
