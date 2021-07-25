c4788c5416f780ae05f648c68b0f4f00e0f3ba03
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-25 02:52:09 -0400

    Allow Camera Associations to be changed via JSON

--- TuningShops/Cameras/CameraManager.cs
+++ TuningShops/Cameras/CameraManager.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using TuningShops.Core;
 
 namespace TuningShops.Cameras
 {
@@ -13,6 +14,7 @@ namespace TuningShops.Cameras
     {
         #region Fields
 
+        private static readonly Dictionary<string, Guid> associations = new Dictionary<string, Guid>();
         private static readonly Dictionary<Guid, CameraCore> cameras = new Dictionary<Guid, CameraCore>();
 
         #endregion
@@ -49,6 +51,26 @@ namespace TuningShops.Cameras
                 }
             }
         }
+        private static void PopulateAssociations()
+        {
+            associations.Clear();
+
+            string path = Path.Combine(TuningShops.location, "Cameras", "Association.json");
+
+            try
+            {
+                string contents = File.ReadAllText(path);
+                Dictionary<string, Guid> @new = JsonConvert.DeserializeObject<Dictionary<string, Guid>>(contents);
+                foreach (KeyValuePair<string, Guid> pair in @new)
+                {
+                    associations.Add(pair.Key, pair.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Notification.Show($"~o~Warning~s~: Unable to load Camera Associations:\n{ex.Message}");
+            }
+        }
 
         #endregion
 
@@ -63,12 +85,32 @@ namespace TuningShops.Cameras
             cameras.Add(Guid.Empty, new GeneralCamera());
             PopulateSpecific<BoneCameraPosition>(Path.Combine(TuningShops.location, "Cameras", "Bone"));
             PopulateSpecific<ModCameraPosition>(Path.Combine(TuningShops.location, "Cameras", "Mod"));
+            PopulateAssociations();
         }
         /// <summary>
         /// Gets a Camera with a specific ID.
         /// </summary>
         /// <param name="id">The ID of the camera.</param>
         public static CameraCore Get(Guid id) => cameras.TryGetValue(id, out CameraCore camera) ? camera : cameras[Guid.Empty];
+        /// <summary>
+        /// Gets a Camera for the specified Mod Menu.
+        /// </summary>
+        /// <param name="base">The menu to use as a base.</param>
+        public static CameraCore Get(BaseType @base)
+        {
+            Type type = @base.GetType();
+            string name = type.FullName;
+
+            if (associations.TryGetValue(name, out Guid guid))
+            {
+                return Get(guid);
+            }
+            else
+            {
+                Notification.Show($"~o~Warning~s~: No Camera Association for {name}");
+                return Get(Guid.Empty);
+            }
+        }
 
         #endregion
     }
