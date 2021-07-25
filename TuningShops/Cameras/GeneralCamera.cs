da3c6bdf9fa950a61ee4d1c459c6edee6a779322
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-24 21:16:44 -0400

    Destroy the general camera before creating a new one

--- TuningShops/Cameras/GeneralCamera.cs
+++ TuningShops/Cameras/GeneralCamera.cs
@@ -1,5 +1,6 @@
 ﻿using GTA;
 using GTA.Math;
+using System;
 
 namespace TuningShops.Cameras
 {
@@ -11,6 +12,13 @@ namespace TuningShops.Cameras
         /// <inheritdoc/>
         public override void Create(Vehicle vehicle)
         {
+            if (vehicle == null)
+            {
+                throw new ArgumentNullException(nameof(vehicle));
+            }
+
+            Destroy();
+
             (_, Vector3 frontTopRight) = vehicle.Model.Dimensions;
             Vector3 relPos = new Vector3(-frontTopRight.X, frontTopRight.Y, frontTopRight.Z);
             Vector3 camPos = vehicle.GetOffsetPosition(new Vector3(relPos.X - 8.262f, relPos.Y - 3.401f, relPos.Z + 0.048f));
