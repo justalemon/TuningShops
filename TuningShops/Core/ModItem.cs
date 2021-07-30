82549fec84b940d839a0bd05aaac3c7af695a91e
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-29 22:53:26 -0400

    Added base item to store the value of the mod

---
+++ TuningShops/Core/ModItem.cs
@@ -0,0 +1,28 @@
+﻿using LemonUI.Menus;
+
+namespace TuningShops.Core
+{
+    /// <summary>
+    /// The basic item for all of the modifications.
+    /// </summary>
+    public class ModItem : NativeItem
+    {
+        #region Properties
+
+        /// <summary>
+        /// The value of this modification.
+        /// </summary>
+        public virtual int Value { get; }
+
+        #endregion
+
+        #region Properties
+
+        public ModItem(string name, string description, int value) : base(name, description, $"${value}")
+        {
+            Value = value;
+        }
+
+        #endregion
+    }
+}
