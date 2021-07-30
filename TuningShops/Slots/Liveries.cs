aa02c12bddffba45135c28b716bab0e705f48d2c
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-29 23:38:44 -0400

    Update liveries for the new item format

--- TuningShops/Slots/Liveries.cs
+++ TuningShops/Slots/Liveries.cs
@@ -1,9 +1,7 @@
 ﻿using GTA;
 using GTA.Native;
-using LemonUI.Menus;
-using System;
-using System.Reflection;
 using TuningShops.Core;
+using TuningShops.Items;
 
 namespace TuningShops.Slots
 {
@@ -53,58 +51,14 @@ namespace TuningShops.Slots
 
             for (int i = -1; i < Function.Call<int>(Hash.GET_VEHICLE_LIVERY_COUNT, vehicle); i++)
             {
-                Add(new LiveryItem(i == -1 ? "NONE" : Function.Call<string>(Hash.GET_LIVERY_NAME, vehicle, i), i));
+                string label = i == -1 ? "NONE" : Function.Call<string>(Hash.GET_LIVERY_NAME, vehicle, i);
+                Add(new CoreItem(i, Function.Call<string>(Hash._GET_LABEL_TEXT, label), "", 0));
             }
         }
         /// <inheritdoc/>
         public override void SelectCurrent(Vehicle vehicle)
         {
             SelectedIndex = Function.Call<int>(Hash.GET_VEHICLE_LIVERY, vehicle) + 1;
-        }
-
-        #endregion
-
-        #region Item
-
-        /// <summary>
-        /// THe item used to enable a specific livery.
-        /// </summary>
-        public class LiveryItem : NativeItem
-        {
-            #region Properties
-
-            /// <summary>
-            /// The index of the livery.
-            /// </summary>
-            public int Index { get; }
-
-            #endregion
-
-            #region Constructor
-
-            public LiveryItem(string label, int index) : base(Function.Call<string>(Hash._GET_LABEL_TEXT, label))
-            {
-                Index = index;
-                Activated += LiveryItem_Activated;
-            }
-
-            #endregion
-
-            #region Events
-
-            private void LiveryItem_Activated(object sender, EventArgs e)
-            {
-                Vehicle vehicle = Game.Player.Character.CurrentVehicle;
-
-                if (vehicle == null)
-                {
-                    return;
-                }
-
-                Function.Call(Hash.SET_VEHICLE_LIVERY, vehicle, Index);
-            }
-
-            #endregion
         }
 
         #endregion
