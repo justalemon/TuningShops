d8aff14a36b43ec9097aa72f2e1da65c9ed54f39
Hannele Ruiz <justlemoncl@gmail.com>
2021-08-31 18:05:14 -0400

    Update the badges after purchasing an item

--- TuningShops/Core/BaseType.cs
+++ TuningShops/Core/BaseType.cs
@@ -149,6 +149,8 @@ namespace TuningShops.Core
             }
 
             LastValue = item.Index;
+
+            UpdateBadges();
         }
         private void BaseType_Closed(object sender, EventArgs e)
         {
