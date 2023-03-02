3c9bcd373237e245560a8d8dbde98829e2b768bc
Hannele Ruiz <justlemoncl@gmail.com>
2023-03-02 05:50:01 -0300

    Added configurable color for the marker

--- TuningShops/Configuration.cs
+++ TuningShops/Configuration.cs
@@ -7,5 +7,25 @@ namespace TuningShops
     /// </summary>
     public class Configuration : ConfigurationCore
     {
+        /// <summary>
+        /// The Red component for the Marker Color.
+        /// </summary>
+        [JsonProperty("marker_r")]
+        public byte MarkerColorR { get; set; } = 128;
+        /// <summary>
+        /// The Green component for the Marker Color.
+        /// </summary>
+        [JsonProperty("marker_g")]
+        public byte MarkerColorG { get; set; }
+        /// <summary>
+        /// The Blue component for the Marker Color.
+        /// </summary>
+        [JsonProperty("marker_b")]
+        public byte MarkerColorB { get; set; } = 128;
+        /// <summary>
+        /// The Alpha or Transparency component for the Marker Color.
+        /// </summary>
+        [JsonProperty("marker_a")]
+        public byte MarkerColorA { get; set; } = 200;
     }
 }
