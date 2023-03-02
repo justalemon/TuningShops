b7c0defd9ea9056ba920bddb253ce3d2f7055bbe
Hannele Ruiz <justlemoncl@gmail.com>
2023-03-02 07:41:09 -0300

    Added Bulletproof tires option

---
+++ TuningShops/Menus/Bulletproof.cs
@@ -0,0 +1,46 @@
+﻿using GTA;
+using GTA.Native;
+using TuningShops.Core;
+
+namespace TuningShops.Menus
+{
+    /// <summary>
+    /// Button used to enable and disable the bulletproof tires.
+    /// </summary>
+    internal class Bulletproof : StoreItem
+    {
+        #region Properties
+
+        /// <summary>
+        /// If this item activates or deactivates the bulletproof tires.
+        /// </summary>
+        public bool Activation { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public Bulletproof(bool activation) : base(activation ? "Bulletproof Tires" : "Standard Tires", activation ? 4000 : 350)
+        {
+            Activation = activation;
+        }
+
+        #endregion
+
+        #region Functions
+
+        public override void Apply()
+        {
+            Vehicle vehicle = Game.Player.Character.CurrentVehicle;
+
+            if (vehicle == null)
+            {
+                return;
+            }
+
+            Function.Call(Hash.SET_VEHICLE_TYRES_CAN_BURST, vehicle, Activation);
+        }
+
+        #endregion
+    }
+}
---
+++ TuningShops/Slots/LSCBulletproof.cs
@@ -0,0 +1,58 @@
+﻿using GTA;
+using GTA.Native;
+using TuningShops.Core;
+using TuningShops.Menus;
+
+namespace TuningShops.Slots
+{
+    internal class LSCBulletproof : ModificationSlot<bool>
+    {
+        #region Properties
+
+        /// <inheritdoc/>
+        public override bool CanBeUsed
+        {
+            get
+            {
+                Vehicle vehicle = Game.Player.Character.CurrentVehicle;
+
+                if (vehicle == null)
+                {
+                    return false;
+                }
+
+                return vehicle.Wheels.Count > 0;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public LSCBulletproof() : base("Bulletproof Tires")
+        {
+            StoreItem n = new Bulletproof(false);
+            StoreItem y = new Bulletproof(true);
+
+            Add(n);
+            Add(y);
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <inheritdoc/>
+        public override void SelectCurrent()
+        {
+            SelectedIndex = Function.Call<bool>(Hash.GET_VEHICLE_TYRES_CAN_BURST, Game.Player.Character.CurrentVehicle) ? 1 : 0;
+            UpdateBadges();
+        }
+        /// <inheritdoc/>
+        public override void Repopulate()
+        {
+        }
+
+        #endregion
+    }
+}
