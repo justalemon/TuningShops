aaf2b1a2d278456fc0af4121216867f39af9af61
Hannele Ruiz <justlemoncl@gmail.com>
2023-11-14 19:44:20 -0300

    Added custom item for the neon colors

---
+++ TuningShops/Menus/NeonColorItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using GTA;
+using GTA.Native;
+
+namespace TuningShops.Menus
+{
+    /// <summary>
+    /// Item used to set custom neon colors.
+    /// </summary>
+    public class NeonColorItem : CustomColorItem
+    {
+        #region Constructors
+
+        /// <inheritdoc/>
+        public NeonColorItem(Color color, string name) : base(color, name, 650)
+        {
+        }
+
+        #endregion
+        
+        #region Functions
+
+        /// <summary>
+        /// Applies the neon color.
+        /// </summary>
+        public override void Apply()
+        {
+            Vehicle playerVehicle = Game.Player.Character.CurrentVehicle;
+
+            if (playerVehicle == null)
+            {
+                return;
+            }
+
+            Function.Call(Hash.SET_VEHICLE_NEON_COLOUR, playerVehicle.Handle, Color.R, Color.G, Color.B);
+        }
+        
+        #endregion
+    }
+}
